a332d356528f3f567ec74da68bb37b8046e92067
Admin <Admin@DESKTOP-QI6H2EA>
2023-04-10 22:15:04 +0200

    Nove anotacije @Embeded,@Enumerated,@ElementCollection, Komanda save

--- hibernateProgramerskaFirma/src/glavni/GlavnaFirma.java
+++ hibernateProgramerskaFirma/src/glavni/GlavnaFirma.java
@@ -1,21 +1,56 @@
 package glavni;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import model.Adresa;
+import model.Contact;
+import model.Rola;
+import model.UserDetails;
+
 public class GlavnaFirma {
 
 	public static void main(String[] args) {
+		
+		UserDetails user = new UserDetails();
+		user.setIme("Nenad");
+		user.setPrezime("Nenadovic");
+		
+		Adresa adresa = new Adresa();
+		adresa.setDrzava("Srbija");
+		adresa.setGrad("Beograd");
+		adresa.setUlica("Terazije 22");
+		adresa.setPostanskiBroj("11000");
+		
+		user.setAdresa(adresa);
+		user.setRola(Rola.USER);
+		
+		Contact kontakt1 = new Contact();
+		kontakt1.setTelefon("064111222");
+		kontakt1.setEmail("prviEmail");
+		
+		Contact kontakt2 = new Contact();
+		kontakt2.setTelefon("064222333");
+		kontakt2.setEmail("drugiEmail");
+		
+		List<Contact> kontakti = new ArrayList<Contact>();
+		kontakti.add(kontakt1);
+		kontakti.add(kontakt2);
+		
+		user.setKontakti(kontakti);
 		
 		//izgradili smo fabriku
 		SessionFactory factory = new Configuration().configure().buildSessionFactory();
 			// preuzimamo sesiju iz fabrike sesija
 			Session sesija = factory.openSession();
 				sesija.beginTransaction();	
-					try {
-						
+					try {	
 						// odradi operaciju
+						sesija.save(user);
 						sesija.getTransaction().commit();
 					}catch (Exception e) {
 						sesija.getTransaction().rollback();
